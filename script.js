3ef67af56635714290b32ded369ca1e1769a4960
Alliya dhiraj <alliyadhirajsubhahchandani@gmail.com>
2023-01-30 13:54:30 +0530

    trainers

--- script.js
+++ script.js
@@ -46,22 +46,18 @@ for (let i = 0; i <= overlay.length - 1; i++) {
 
 // trainer section
 
-let trainer = document.querySelectorAll(".trainers");
+let trainersDetails = document.querySelectorAll(".trainer-details");
+let trainers = document.querySelectorAll(".training")
 
-for (let i = 0; i <= trainer.length - 1; i++) {
+for (let i = 0; i <= trainers.length -1; i++) {
 //   //visible event.
-  trainer[i].addEventListener("mouseover", (e) => {
-    // console.log(e.target.children)
-    e.target.children[i].style.visibility = "visible";
-
-  //   e.target.parentNode.children[3].addEventListener("mouseover", () => {
-  //     e.target.parentNode.children[3].style.visibility = "visible";
-  //   });
+  trainers[i].addEventListener("mouseover", (e) => {
+    (trainersDetails[i].style.visibility= "visible");
   });
 
-  //Hide event.
-  trainer[i].addEventListener("mouseout", (e) => {
-    e.target.children[i].style.visibility = "hidden";
+  // Hide event.
+  trainers[i].addEventListener("mouseout", (e) => {
+    (trainersDetails[i].style.visibility= "hidden");
   
-});
+ });
 }
